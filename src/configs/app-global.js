0cc85f62133f1c465be565ff18aa9ccf72344541
Ravindu Dhananjaya <52064318+ravindudhananjaya@users.noreply.github.com>
2024-08-15 12:25:27 +0900

    add

---
+++ src/configs/app-global.js
@@ -0,0 +1,43 @@
+export const PROJECT_NAME = 'Demand24 marketplace';
+export const BASE_URL = 'https://api.demand24.org';
+export const WEBSITE_URL = 'https://demand24.org';
+export const api_url = BASE_URL + '/api/v1/';
+export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
+export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
+export const IMG_URL = '';
+export const MAP_API_KEY = 'AIzaSyBbfnvObzwcs6OLXOmSaG9CtIhwjmBwLTQ';
+export const export_url = BASE_URL + '/storage/';
+export const example = BASE_URL + '/';
+export const defaultCenter = { lat: 40.7127281, lng: -74.0060152 };
+
+export const VAPID_KEY =
+  'BD1MvrxUSshXYP2zI4TZjz5dpd9BI7YH7uWFBCZQL5lHUTR0XM41SouZ7HiBBDvJ8_Aps7FNkq1oinVyMuLiM3s';
+export const API_KEY = 'AIzaSyDAPFJegc9HiNJbX-KHauDUR3_vVljfLYc';
+export const AUTH_DOMAIN = 'ibeauty-e83da.firebaseapp.com';
+export const PROJECT_ID = 'ibeauty-e83da';
+export const STORAGE_BUCKET = 'ibeauty-e83da.appspot.com';
+export const MESSAGING_SENDER_ID = '592543327679';
+export const APP_ID = '1:592543327679:web:e90503106ff0fbd86f6d0a';
+export const MEASUREMENT_ID = 'G-6XEEY7TH0K';
+export const DYNAMIC_LINK_DOMAIN = 'https://ibeautyapp.page.link';
+export const ANDROID_PACKAGE_NAME = 'org.uzmart';
+export const IOS_BUNDLE_ID = 'com.gshop';
+
+export const RECAPTCHASITEKEY = '6LcF5o8pAAAAANGuSoWI5qDryYPaA4OpB3DHS28T';
+
+export const DEMO_SELLER = 107; // seller_id
+export const DEMO_SELLER_UUID = '3566bdf6-3a09-4488-8269-70a19f871bd0'; // seller_id
+export const DEMO_SHOP = 501; // seller_id
+export const DEMO_DELIVERYMAN = 106; // deliveryman_id
+export const DEMO_MANEGER = 114; // maneger_id
+export const DEMO_MODERATOR = 297; // moderator_id
+export const DEMO_ADMIN = 501; // administrator_id
+export const DEMO_WAITER = 108; // waiter_id
+
+export const SUPPORTED_FORMATS = [
+  'image/jpg',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/svg',
+];
